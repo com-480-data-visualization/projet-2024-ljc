80d157091a6e9a892328054f614ccf07a938b06b
Tavernier <clara.tavernier@epfl.ch>
2024-04-16 08:26:11 +0200

    Create squeleton of the website

---
+++ website/script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor: ['#ffcdf9', '#fffecd', '#ffcdf9', '#fffecd'],
+    navigation: true,
+    slidesNavigation: true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'],
+    navigationTooltips: ['Title', 'First Section', 'Slides', 'Third Section'],
+});
