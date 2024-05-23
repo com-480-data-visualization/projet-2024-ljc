5108841bb7be0382bf3b92d5738e3e46b9759fd6
Louise Anne Marie Font <louise.font@gmail.ch>
2024-05-23 09:23:15 +0200

    for julie to merge

---
+++ website/js/co2-tshirt.js
@@ -0,0 +1,103 @@
+console.log("I'm in the co2.js file :)");
+
+function whenDocumentLoaded(action) {
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", action);
+	} else {
+		// `DOMContentLoaded` already fired
+		action();
+	}
+}
+
+function drawTimeChart(dataset) {
+    console.log('drawTimeChart')
+    let ctx = document.getElementById('co2tshirt'); 
+    
+    //let's make the data usable
+    //let = datasets [];
+    
+    let col = Object.keys(dataset[0]);
+    let d = { 'year' : [], 'col1':[], 'col2':[], 'col2':[], 'col3':[], 'col4':[]}
+    dataset.forEach((row) => {
+        d['year'].push(row[col[0]])
+        d['col1'].push(row[col[1]])
+        d['col2'].push(row[col[2]])
+        d['col3'].push(row[col[3]])
+        d['col4'].push(row[col[4]])
+    });
+    console.log('just before chart')
+    new Chart(ctx, {
+        type: "line",
+        data: {
+            labels : d.year,
+            datasets : [{
+                data : d.col1,
+                fill : false,
+                label : col[1],
+                borderColor : 'red',
+                yAxisID : 'y1',
+            },{
+                data : d.col2,
+                label : col[2],
+                yAxisID : 'y1',
+                borderColor : 'blue',
+                fill : false,
+            },{
+                data : d.col3,
+                label : col[3],
+                fill : false,
+                borderColor : 'green',
+                yAxisID : 'y1',
+            },{
+                data : d.col4,
+                label : col[4],
+                fill : false,
+                borderColor : 'yellow',
+                yAxisID : 'y2',
+            }]
+        },
+        options: {
+            layout: {
+                padding: 60
+            },
+            scales : {
+                yAxes: [{
+                    id: 'y1',
+                    type:'linear',
+                    position:'left',
+                    scaleLabel : {
+                        display:true,
+                        labelString:'Water, CO2, Land',
+                    },
+                    ticks : {
+                        min: 2,
+                        max : 5,
+                        stepSize: 0.5,
+                    }
+                },{
+                    id: 'y2',
+                    type:'linear',
+                    position:'right',
+                    scaleLabel : {
+                        display:true,
+                        labelString:'Energy',
+                    },
+                    ticks : {
+                        min: 12,
+                        max: 21,
+                        stepSize: 1.5,
+                    }
+                }
+                ]
+            }
+        }
+      });
+}
+
+whenDocumentLoaded(() => {
+    console.log('loaded hey')
+
+    d3.csv('data/final_short.csv').then((data) => {
+        drawTimeChart(data)
+    })
+});
--- website/js/time.js
+++ website/js/time.js
@@ -1,4 +1,5 @@
 // just to check i'm entering the correct folder
+/*
 console.log("I'm in the time.js file :)");
 
 function whenDocumentLoaded(action) {
@@ -37,44 +38,63 @@ function drawTimeChart(dataset) {
             labels : d.year,
             datasets : [{
                 data : d.col1,
+                fill : false,
                 label : col[1],
+                borderColor : 'red',
                 yAxisID : 'y1',
             },{
                 data : d.col2,
                 label : col[2],
-                yAxisID : 'y2',
+                yAxisID : 'y1',
+                borderColor : 'blue',
+                fill : false,
             },{
                 data : d.col3,
                 label : col[3],
-                yAxisID : 'y3',
+                fill : false,
+                borderColor : 'green',
+                yAxisID : 'y1',
             },{
                 data : d.col4,
                 label : col[4],
-                yAxisID : 'y4',
+                fill : false,
+                borderColor : 'yellow',
+                yAxisID : 'y2',
             }]
         },
         options: {
             layout: {
-                padding: 20
+                padding: 60
             },
             scales : {
                 yAxes: [{
                     id: 'y1',
                     type:'linear',
                     position:'left',
+                    scaleLabel : {
+                        display:true,
+                        labelString:'Water, CO2, Land',
+                    },
+                    ticks : {
+                        min: 2,
+                        max : 5,
+                        stepSize: 0.5,
+                    }
                 },{
                     id: 'y2',
                     type:'linear',
-                    position:'left',
-                },{
-                    id: 'y3',
-                    type:'linear',
                     position:'right',
-                },{
-                    id: 'y4',
-                    type:'linear',
-                    position:'right',
-                }]
+                    scaleLabel : {
+                        display:true,
+                        labelString:'Energy',
+                    },
+                    ticks : {
+                        min: 12,
+                        max: 21,
+                        stepSize: 1.5,
+                    }
+                }
+                ]
             }
         }
       });
@@ -83,7 +103,142 @@ function drawTimeChart(dataset) {
 whenDocumentLoaded(() => {
     console.log('loaded hey')
 
-    d3.csv('data/final.csv').then((data) => {
+    d3.csv('data/final_short.csv').then((data) => {
         drawTimeChart(data)
     })
+});
+*/
+
+console.log("I'm in the time.js file :)");
+
+function whenDocumentLoaded(action) {
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", action);
+    } else {
+        // `DOMContentLoaded` already fired
+        action();
+    }
+}
+
+function drawTimeChart(dataset, startYear, endYear) {
+    console.log('drawTimeChart');
+    let ctx = document.getElementById('timeChart').getContext('2d');
+
+    // Filter dataset based on selected years
+    console.log(dataset)
+    let filteredData = dataset.filter(function(row)  {
+        return row.Year >= startYear && row.Year <= endYear;
+    }); 
+
+
+    console.log(filteredData)
+    let col = Object.keys(dataset[0]);
+    let d = { 'year': [], 'col1': [], 'col2': [], 'col3': [], 'col4': [] };
+    filteredData.forEach((row) => {
+        d['year'].push(row[col[0]]);
+        d['col1'].push(row[col[1]]);
+        d['col2'].push(row[col[2]]);
+        d['col3'].push(row[col[3]]);
+        d['col4'].push(row[col[4]]);
+    });
+
+    console.log('just before chart');
+    console.log(d.year)
+    new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: d.year,
+            datasets: [{
+                data: d.col1,
+                fill: false,
+                label: col[1],
+                borderColor: 'red',
+                yAxisID: 'y1',
+            }, {
+                data: d.col2,
+                label: col[2],
+                yAxisID: 'y1',
+                borderColor: 'blue',
+                fill: false,
+            }, {
+                data: d.col3,
+                label: col[3],
+                fill: false,
+                borderColor: 'green',
+                yAxisID: 'y1',
+            }, {
+                data: d.col4,
+                label: col[4],
+                fill: false,
+                borderColor: 'yellow',
+                yAxisID: 'y2',
+            }]
+        },
+        options: {
+            layout: {
+                padding: 20
+            },
+            scales: {
+                x: {
+                    stacked: false
+                },
+                yAxes: [{
+                    id: 'y1',
+                    type: 'linear',
+                    position: 'left',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Water, CO2, Land',
+                    },
+                    ticks: {
+                        min: 2,
+                        max: 5,
+                        stepSize: 0.5,
+                    }
+                }, {
+                    id: 'y2',
+                    type: 'linear',
+                    position: 'right',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Energy',
+                    },
+                    ticks: {
+                        min: 12,
+                        max: 21,
+                        stepSize: 1.5,
+                    }
+                }]
+            }
+        }
+    });
+}
+
+whenDocumentLoaded(() => {
+    console.log('loaded hey');
+
+    d3.csv('data/final_short.csv').then((data) => {
+        // Convert year strings to numbers
+        data.forEach(d => {
+            d.year = +d.year;
+        });
+
+        // Initial draw of the chart
+        drawTimeChart(data, 1965, 2014);
+
+        var slider = document.getElementById('slider');
+
+        noUiSlider.create(slider, {
+            start: [2500, 8500], // Initial values
+            connect: true,
+            step : 1,
+            range: {    
+                'min': 0,
+                'max': 10000
+            }
+        }
+    );
+
+
+    });
 });
