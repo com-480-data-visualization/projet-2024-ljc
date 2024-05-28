1b01971ae8436cf931655f0a8f9449d93a2ed96f
ctaverni <clara.tavernier@epfl.ch>
2024-05-28 20:38:31 +0200

    Made disapear the waste box when popup, and made bold some word in co2 popup

--- website/js/script.js
+++ website/js/script.js
@@ -1,30 +1,44 @@
-
+function hideOrShowWasteCounter() {
+    if (document.getElementById('counter').style.display == 'none') {
+        document.getElementById('counter').style.display = 'block';
+    } else {
+        document.getElementById('counter').style.display = 'none';
+    }
+}
 
 document.getElementById('part1').addEventListener('click', function() {
     document.getElementById('popup-part1').style.display = 'block'; 
+    hideOrShowWasteCounter();
 });
 
 document.getElementById('part2').addEventListener('click', function() {
     document.getElementById('popup-part2').style.display = 'block'; 
+    hideOrShowWasteCounter();
 });
 
 document.getElementById('part3').addEventListener('click', function() {
     document.getElementById('popup-part3').style.display = 'block'; 
+    hideOrShowWasteCounter();
 });
 
 document.getElementById('part4').addEventListener('click', function() {
-    document.getElementById('popup-part4').style.display = 'block'; 
+    document.getElementById('popup-part4').style.display = 'block';
+    hideOrShowWasteCounter();
 });
 
 window.addEventListener('click', function(event) {
     if (event.target === document.getElementById('popup-part1')) {
         document.getElementById('popup-part1').style.display = 'none';
+        hideOrShowWasteCounter();
     } else if (event.target === document.getElementById('popup-part2')){
         document.getElementById('popup-part2').style.display = 'none';
+        hideOrShowWasteCounter();
     } else if (event.target === document.getElementById('popup-part3')){
         document.getElementById('popup-part3').style.display = 'none';
+        hideOrShowWasteCounter();
     } else if (event.target === document.getElementById('popup-part4')){
         document.getElementById('popup-part4').style.display = 'none';
+        hideOrShowWasteCounter();
     }
 });
 
