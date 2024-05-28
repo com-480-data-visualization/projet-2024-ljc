50d1362ab620b697c80905470773630e824323da
Louise Anne Marie Font <louise.font@gmail.ch>
2024-05-28 11:16:47 +0200

    new style

--- website/js/time.js
+++ website/js/time.js
@@ -47,26 +47,30 @@ function drawTimeChart(dataset, startYear = 1965, endYear = 2014) {
                 data : d.col1,
                 fill : false,
                 label : col[1],
-                borderColor : 'red',
+                borderColor : getComputedStyle(document.documentElement).getPropertyValue('--blue-color').trim(),
                 yAxisID : 'y1',
+                xAxisID : 'x1'
             },{
                 data : d.col2,
                 label : col[2],
                 yAxisID : 'y1',
-                borderColor : 'blue',
+                borderColor : getComputedStyle(document.documentElement).getPropertyValue('--green-color').trim(),
                 fill : false,
+                xAxisID : 'x1'
             },{
                 data : d.col3,
                 label : col[3],
                 fill : false,
-                borderColor : 'green',
+                borderColor : getComputedStyle(document.documentElement).getPropertyValue('--orange-color').trim(),
                 yAxisID : 'y1',
+                xAxisID : 'x1'
             },{
                 data : d.col4,
                 label : col[4],
                 fill : false,
-                borderColor : 'yellow',
+                borderColor : getComputedStyle(document.documentElement).getPropertyValue('--purple-color').trim(),
                 yAxisID : 'y2',
+                xAxisID : 'x1'
             }]
         },
         options: {
@@ -86,6 +90,9 @@ function drawTimeChart(dataset, startYear = 1965, endYear = 2014) {
                     max : 5,
                     ticks : {
                         stepSize: 0.5,
+                    },
+                    grid: {
+                        display:true
                     }
                 },
                 y2: {
@@ -100,9 +107,26 @@ function drawTimeChart(dataset, startYear = 1965, endYear = 2014) {
                     max: 21,
                     ticks : {
                         stepSize: 1.5,
+                    },
+                    grid: {
+                        display:true
+                    }
+                },
+                x1: {
+                    grid:{
+                        display:false,
+                    },
+                    title:{
+                        display:true,
+                        text:'Year',
+                    },
+                    ticks : {
+                        stepSize:1,
                     }
                 }
-            }
+                
+                }    
+            
         }
       });
 }
@@ -124,10 +148,6 @@ let sliderMaxValue = document.getElementById("slider-1").max;
 let sliderMinValue = document.getElementById("slider-1").min;
 let sliderRange = document.getElementById('slider-1').max - document.getElementById('slider-1').min;
 
-console.log('range')
-console.log(sliderRange)
-
-
 function slideOne(){
     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
         sliderOne.value = parseInt(sliderTwo.value) - minGap;
@@ -143,41 +163,20 @@ function slideTwo(){
     fillColor();
 }
 function fillColor(){
-    console.log('calling fill color')
     percent1 = ((sliderOne.value - sliderMinValue)/ sliderRange) * 100;
-
     percent2 = ((sliderTwo.value - sliderMinValue) / sliderRange) * 100;
-    console.log(percent2)
-    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #81b2b7 ${percent1}% , #81b2b7 ${percent2}%, #dadae5 ${percent2}%)`;
 }
 
 sliderOne.addEventListener('input', slideOne);
 sliderTwo.addEventListener('input', slideTwo);
-
 /*
-whenDocumentLoaded(() => {
-    console.log('loaded hey')
-    window.onload = function(){
-    slideOne();
-    slideTwo();
-}
-
-
-let sliderOne = document.getElementById("slider-1");
-let sliderTwo = document.getElementById("slider-2");
-let displayValOne = document.getElementById("range1");
-let displayValTwo = document.getElementById("range2");
-let minGap = 0;
-let sliderTrack = document.querySelector(".slider-track");
-let sliderMaxValue = document.getElementById("slider-1").max;
-*/
 function slideOne() {
     if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
         sliderOne.value = parseInt(sliderTwo.value) - minGap;
     }
     displayValOne.textContent = sliderOne.value;
     displayValOne.style.left = `${(sliderOne.value - sliderOne.min) / (sliderOne.max - sliderOne.min) * 100}%`;
-    console.log('in slide one')
     fillColor();
     updateChart();
 }
@@ -191,16 +190,17 @@ function slideTwo() {
     fillColor();
     updateChart();
 }
-
-
+*/
 whenDocumentLoaded(() => {
+    console.log('louise')
     slideOne()
     slideTwo()
     sliderOne.addEventListener('input', slideOne);
     sliderTwo.addEventListener('input', slideTwo);
-    console.log('louie')
+    console.log('before d3')
 
     d3.csv('data/final_short.csv').then((data) => {
+        console.log('what is going on')
         drawTimeChart(data, sliderOne.value, sliderTwo.value)
             document.getElementById('slider-1').addEventListener('input', () => {
             canvas = document.getElementById('canvas');
