7e533e1a1b49fcd8d8d226b2686d82e11751b9d0
Kojuly2 <julie.korber@epfl.ch>
2024-05-31 14:48:05 +0200

    clean comments

--- website/js/textile.js
+++ website/js/textile.js
@@ -7,16 +7,18 @@ function whenDocumentLoaded(action) {
 }
 
 function radarChart(row_fabric) {
+    // get the id from the index.html
     let ctx = document.getElementById('radarChart');
 
+    // Prepare data needed for the plot
     let datasets = [];
 
     row_fabric.forEach((row) => {
-        var dataset = {
+        dataset = {
             label: row.fabric,
             data: [row[lab_[0]], row[lab_[1]], row[lab_[2]]],
             backgroundColor: getColorWithOpacity(row.color, 0.2),
-            borderColor: getColorWithOpacity(row.color, 1),
+            borderColor: getColorWithOpacity(row.color, 1), 
             borderWidth: 1
         };
         datasets.push(dataset);
@@ -53,42 +55,55 @@ function createDiagonalPattern(type, color = 'black') {
         shape.width = 10
         shape.height = 10
         let c = shape.getContext('2d')
+
+        // Draw 1st line of the shape 
         c.strokeStyle = color
         c.beginPath()
         c.moveTo(2, 0)
         c.lineTo(10, 8)
         c.stroke()
+
+        // Draw 2nd line of the shape 
         c.beginPath()
         c.moveTo(0, 8)
         c.lineTo(2, 10)
         c.stroke()
+
+        // Create the pattern from the shape
         return c.createPattern(shape, 'repeat')
     } else if(type == 'natural'){
         return getColorWithOpacity(color, 0.2)
+    } else {
+        console.log('Unknown type of fabric')
     };
         
 }
 
 function barChart(row_fabric, dataLabel, first = false) {
+    // Get the canvas element
     let canvas = document.getElementById('comparisonFabric');
-    
+    // Get the 2D context of the canvas
     let ctxBar = canvas.getContext('2d');
 
+    // Prepare data needed for the plot
     let datasets = [];
     row_fabric.forEach((row) => {
         let dataset = {
             label: row.fabric,
             data: [row[dataLabel]],
             backgroundColor: createDiagonalPattern(row.type, row.color), 
-            borderColor: getColorWithOpacity(row.color, 1),
+            borderColor: getColorWithOpacity(row.color, 1), 
             borderWidth: 1
         };
         datasets.push(dataset);
     });
 
+    // Sorts the datasets by data values
     datasets = datasets.sort((a, b) => b.data - a.data);
     
+    // When the chart already exsits, we need to update it instead of creating a new one 
     if (first) {
+        // Initialize the bar chart
         myChart = new Chart(ctxBar, {
                                 type: 'bar',
                                 data: {
@@ -100,7 +115,7 @@ function barChart(row_fabric, dataLabel, first = false) {
                                         duration: 700, 
                                         easing: 'easeInOutQuad'
                                     },
-                                    plugins: {
+                                    plugins: { 
                                         legend: {
                                             display: true,
                                             position: 'top', 
@@ -108,15 +123,15 @@ function barChart(row_fabric, dataLabel, first = false) {
                                     },
                                     layout: {
                                         padding: {
-                                            top: 20,
+                                            top: 20, 
                                             bottom: 20
                                         }
                                     }
                                 }
                     });
     } 
+    // Update the existing chart
     else if (myChart) {
-
         myChart.data.labels = [dataLabel];
         myChart.data.datasets = datasets
         myChart.update();
@@ -145,11 +160,13 @@ whenDocumentLoaded(() => {
         getComputedStyle(document.documentElement).getPropertyValue('--pink2-color').trim()]
 
         textiles.forEach((textile, index) => {
+            // Assign color from the color scale
             textile.color = d3.rgb(colorScale[index]);
         });
 
+        // Bar plots
         barChart(textiles, lab_[0], true);
-
+        // Add the buttons
         document.getElementById('waterButton').addEventListener('click', () => {
             barChart(textiles, lab_[0]);
         });
@@ -162,8 +179,8 @@ whenDocumentLoaded(() => {
             barChart(textiles, lab_[2]);
         });
 
+        // Calculate percentages
         let textiles_percentage = JSON.parse(JSON.stringify(textiles))
-
         for (let i = 0; i < lab_.length; i++) {
             const max_lab = Math.max(...Object.values(textiles_percentage).map(item => item[lab_[i]]).filter(val => !isNaN(val)));
             textiles_percentage.forEach((row) => {
@@ -172,10 +189,7 @@ whenDocumentLoaded(() => {
             })
 
           }
+        // radar Chart
         radarChart(textiles_percentage);
-
-
     });
-
 });
-
